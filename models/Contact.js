21019146c34ecd75b8b2056587ac45d8fd6bd531
Alain Guillon <a.guillon@it-students.fr>
2023-01-28 21:53:53 +0100

    :v 0.2.0 - Create:Model
    Création du model Contact et mise à jour de quelques name absents des formulaires d'ajout ou modification d'un contact

---
+++ models/Contact.js
@@ -0,0 +1,79 @@
+/**
+ * ================================================================
+ * Modules
+ * ================================================================
+ */
+const mongoose = require("mongoose");
+
+/**
+ * ================================================================
+ * Schema
+ * ================================================================
+ */
+const ContactSchema = mongoose.Schema(
+  {
+    firstname: {
+      type: String,
+      minLength: 3,
+      maxLength: 50,
+      required: true,
+    },
+    lastname: {
+      type: String,
+      minLength: 3,
+      maxLength: 50,
+      required: true,
+    },
+    email: {
+      type: String,
+      unique: true,
+    },
+    phone: {
+      type: Number,
+      min: 10,
+      max: 10,
+      required: true,
+    },
+    sector: {
+      type: String,
+      required: true,
+    },
+    society: {
+      type: String,
+      required: true,
+    },
+    streetNumber: {
+      type: Number,
+      min: 1,
+      max: 9999,
+      required: true,
+    },
+    typeStreet: {
+      type: String,
+      required: false,
+    },
+    address: {
+      type: String,
+      required: true,
+    },
+    codePostal: {
+      type: Number,
+      required: true,
+      min: 1,
+      max: 99999,
+    },
+  },
+  {
+    timestamps: {
+      createdAt: "created_at",
+      updatedAt: "updated_at",
+    },
+  }
+);
+
+/**
+ * ================================================================
+ * Export
+ * ================================================================
+ */
+module.exports = mongoose.model("contacts", ContactSchema);

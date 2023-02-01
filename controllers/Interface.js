7e348fb7f0055888e3e39d4163ae9fe12d8bacd0
alain guillon <a.guillon@it-students.fr>
2023-02-01 09:01:13 +0100

    :v 0.7.2 - Update:Structure
    Reprise du projet pour alléger l'écriture, pertes des messages d'erreurs mais simplification de beaucoup de syntaxe

--- controllers/Interface.js
+++ controllers/Interface.js
@@ -7,67 +7,79 @@ const ContactModel = require("../models/Contact");
 
 /**
  * ================================================================
+ * Data
+ * ================================================================
+ */
+
+// indice tableau d'errorMessage
+let i = 0;
+
+// Tableau errorMessage
+const errorMessage = [];
+
+const data = {
+  errors: {
+    tabError: [] ?? errorMessage,
+  },
+  title: "",
+  contact: {} ?? document,
+  contacts: [] ?? documents,
+};
+
+/**
+ * ================================================================
  * CONTROLLER GET
  * ================================================================
  */
+
+// OK
 const getHome = (req, res, next) => {
   ContactModel.find()
     .then((documents) => {
-      // console.log(documents);
+      let { title, contacts } = data;
+      title = "Accueil";
+      contacts = documents;
+      //console.log(documents, data);
+      //console.log(title, contacts);
       res.status(200).render("pages/home", {
-        data: {
-          head: {
-            title: "Accueil",
-          },
-          body: documents,
-        },
+        title,
+        contacts,
       });
     })
     .catch((err) => next(err.stack));
 };
 
+// PAS OK
 const getCreateContact = (req, res, next) => {
+  let { title, contact, errors } = data;
+  title = "Nouveau contact";
+  errors.tabError = errorMessage.length > 0 ? errorMessage : [];
+  console.log(title, contact, errors, data);
   res.status(200).render("pages/add-contact", {
-    errors: {
-      tabError: [],
-    },
-    data: {
-      head: {
-        title: "Ajout d'un nouveau contact",
-      },
-    },
+    title,
+    contact,
+    errors,
   });
 };
 
+// PAS OK
 const getReadContact = (req, res, next) => {
   res.status(200).render("pages/show-contact", {
-    data: {
-      head: {
-        title: "Un contact",
-      },
-    },
+    title: "Un contact",
   });
 };
 
+// PAS OK
 const getUpdateContact = (req, res, next) => {
-  // Tableau d'erreur
-  const errorMessage = [];
-
-  // i tableau
-  let i = 0;
-
   res.status(200).render("pages/edit-contact", {
     errors: {
       tabError: errorMessage,
     },
-    data: {
-      head: {
-        title: "Edition d'un contact",
-      },
-    },
+    title: "Edition d'un contact",
   });
 };
 
+// OK
 const getDeleteContact = (req, res, next) => {
   if (req.params.id) {
     ContactModel.findById({ _id: req.params.id })
@@ -83,6 +95,7 @@ const getDeleteContact = (req, res, next) => {
   }
 };
 
+// OK
 const getRedirectToHome = (req, res, next) => {
   res.redirect("/");
 };
@@ -93,7 +106,6 @@ const getRedirectToHome = (req, res, next) => {
  * ================================================================
  */
 const postCreateContact = (req, res, next) => {
-  // Data
   const contactObject = {
     lastname: req.body.lastname,
     firstname: req.body.firstname,
@@ -108,144 +120,10 @@ const postCreateContact = (req, res, next) => {
     city: req.body.city,
   };
 
-  // Tableau d'erreur
-  const errorMessage = [];
-
-  // i tableau
-  let i = 0;
-
-  // Lastname
-  if (contactObject.lastname == "") {
-    errorMessage[i] = 'Le champ "Nom" ne peut-être vide';
-    i++;
-  } else if (contactObject.lastname.length < 3) {
-    errorMessage[i] = 'Le champ "Nom" doit faire au minimum 3 caractères';
-    i++;
-  } else if (contactObject.lastname.length > 50) {
-    errorMessage[i] = 'Le champ "Nom" ne peut pas dépasser 50 caractères';
-    i++;
-  } else if (typeof contactObject.lastname != "string") {
-    errorMessage[i] =
-      'Le champ "Nom" doit contenir exclusivement une chaîne de caractère';
-    i++;
-  }
-
-  // Firstname
-  if (contactObject.firstname == "") {
-    errorMessage[i] = 'Le champ "Prénom" ne peut-être vide';
-    i++;
-  } else if (contactObject.firstname.length < 3) {
-    errorMessage[i] = 'Le champ "Prénom" doit faire minimum 3 caractères';
-    i++;
-  } else if (contactObject.firstname.length > 50) {
-    errorMessage[i] = 'Le champ "Prénom" ne peut pas dépasser 50 caractères';
-    i++;
-  } else if (typeof contactObject.firstname != "string") {
-    errorMessage[i] =
-      'Le champ "Prénom" doit contenir exclusivement une chaîne de caractère';
-    i++;
-  }
-
-  // Email
-  if (contactObject.email == "") {
-    errorMessage[i] = 'Le champ "Email" ne peut-être vide';
-    i++;
-  } else if (typeof contactObject.firstname != "string") {
-    errorMessage[i] =
-      'Le champ "Email" doit exclusivement contenir une chaîne de caractère';
-    i++;
-  }
-
-  // Phone
-  if (contactObject.phone == "") {
-    errorMessage[i] = 'Le champ "Téléphone" ne peut-être vide';
-    i++;
-  } else if (contactObject.phone.length != 10) {
-    errorMessage[i] =
-      'Le champ "Téléphone" doit faire exclusivement 10 caractères de type nombre';
-    i++;
-  } else if (typeof contactObject.phone != "string") {
-    errorMessage[i] =
-      'Le champ "téléphone" doit contenir exclusivement un type nombre';
-    i++;
-  }
-
-  // Sector
-  if (contactObject.sector == "") {
-    errorMessage[i] = 'Le champ "Secteur" ne peut-être vide';
-    i++;
-  } else if (typeof contactObject.firstname != "string") {
-    errorMessage[i] =
-      'Le champ "Secteur" doit contenir exclusivement une chaîne de caractère';
-    i++;
-  }
-
-  // Société
-  if (contactObject.society == "") {
-    errorMessage[i] = 'Le champ "Société" ne peut-être vide';
-    i++;
-  } else if (typeof contactObject.society != "string") {
-    errorMessage[i] =
-      'Le champ "Société" doit contenir exclusivement une chaîne de caractère';
-    i++;
-  }
-
-  // StreetNumber
-  if (contactObject.streetNumber == "") {
-    errorMessage[i] = 'Le champ "N°" ne peut-être vide';
-    i++;
-  } else if (contactObject.streetNumber.length < 1) {
-    errorMessage[i] = 'Le champ "N°" doit commencer au numéro 1';
-    i++;
-  } else if (contactObject.streetNumber.length > 9999) {
-    errorMessage[i] = 'Le champ "N°" ne peut pas exéder 9999';
-    i++;
-  } else if (typeof contactObject.streetNumber != "number") {
-    errorMessage[i] = 'Le champ "N°" doit contenir exclusivement un nombre';
-    i++;
-  }
-
-  // Code Postal
-  if (contactObject.codePostal == "") {
-    errorMessage[i] = 'Le champ "Code Postal" ne peut-être vide';
-    i++;
-  } else if (contactObject.codePostal.length < 1) {
-    errorMessage[i] = 'Le champ "Code Postal" doit commencer au numéro 1';
-    i++;
-  } else if (contactObject.codePostal.length > 99999) {
-    errorMessage[i] = 'Le champ "Code Postal" ne peut pas exéder 99999';
-    i++;
-  } else if (typeof contactObject.codePostal != "number") {
-    errorMessage[i] =
-      'Le champ "Code Postal" doit contenir exclusivement un nombre';
-    i++;
-  }
-
-  // City
-  if (contactObject.city == "") {
-    errorMessage[i] = 'Le champ "Ville" ne peut-être vide';
-    i++;
-  }
-
-  // Address
-  if (contactObject.address == "") {
-    errorMessage[i] = 'Le champ "Adresse" ne peut-être vide';
-    i++;
-  }
-
   if (errorMessage.length > 0) {
-    // console.log(errorMessage);
-    res.status(200).render("pages/add-contact", {
-      errors: {
-        tabError: errorMessage,
-      },
-      data: {
-        head: {
-          title: "Ajout d'un nouveau contact",
-        },
-      },
-    });
+    res.status(200).render("pages/add-contact");
   } else if (errorMessage.length == 0) {
+    console.log(contactObject);
     const newContact = new ContactModel(contactObject);
     newContact
       .save()
@@ -424,7 +302,9 @@ const putUpdateContact = (req, res, next) => {
   ContactModel.updateOne(
     { _id: req.request.id },
     {
-      lastname: req.body.lastname,
+      $set: {
+        lastname: req.body.lastname,
+      },
     }
   )
     .then(() => res.redirect("/"))
